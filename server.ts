0015ba2e8298c7210d99dedd064bfb112a651e19
eslam bayome <eslamalaa532@gmail.com>
2025-05-02 11:47:17 +0300

    feat: update environment configuration and add MongoDB connection details

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
-const app = require('./app');
 const dotenv = require('dotenv');
-
+//we have to import dotenv before importing the app
 dotenv.config({ path: './config.env' });
 
-const port = process.env.PORT;
+const app = require('./app');
+
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
