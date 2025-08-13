91384372b2f0c346c56bb91f0e41a928bdcb28b7
eslamBayome <eslam.bayome@asset.com.eg>
2025-08-13 14:23:24 +0300

    populate the review model with the real data

--- models/reviewModel.ts
+++ models/reviewModel.ts
@@ -37,5 +37,16 @@ const reviewSchema = new mongoose.Schema(
     },
   }
 );
-
+reviewSchema.pre(/^find/, function (next) {
+  (this as any)
+    .populate({
+      path: 'user',
+      select: 'name photo',
+    })
+    .populate({
+      path: 'tour',
+      select: 'name',
+    });
+  next();
+});
 export const Review = mongoose.model('Review', reviewSchema);
