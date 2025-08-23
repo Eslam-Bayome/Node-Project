bf2ebcb7b4461ea813073dc5882b06a352a78bdb
eslamBayome <eslam.bayome@asset.com.eg>
2025-08-23 16:37:50 +0300

    create static method to aggregate the avg rating and number of reviews  in reviewModel

--- models/reviewModel.ts
+++ models/reviewModel.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Tour } from './tourModel';
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -37,6 +38,46 @@ const reviewSchema = new mongoose.Schema(
     },
   }
 );
+reviewSchema.statics.calcAvgRatings = async function (tourId) {
+  // we make it static method so we can call the aggregate method in the model
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating.toFixed(2),
+  });
+};
+
+reviewSchema.post('save', function () {
+  (this.constructor as any).calcAvgRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  const document = await (this as any).clone().findOne();
+  (this as any).reviewDoc = document;
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  //  const document = await (this as any).clone().findOne();  does not work here becoasue the query has already executed
+  if ((this as any).reviewDoc) {
+    await ((this as any).reviewDoc.constructor as any).calcAvgRatings(
+      (this as any).reviewDoc.tour
+    );
+  }
+});
+
 reviewSchema.pre(/^find/, function (next) {
   (this as any).populate({
     path: 'user',
