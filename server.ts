4f21d01eb1e82bf5f6b5711cea259342f35204ba
eslamBayome <eslam.bayome@asset.com.eg>
2025-08-28 10:57:11 +0300

    handle uncaught expception and rejection

--- server.ts
+++ server.ts
@@ -1,5 +1,15 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
+process.on('uncaughtException', (err: any) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  //we give the server a chance to finish the request adn after the server killed
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
 //we have to import dotenv before importing the app
 dotenv.config({ path: './config.env' });
 
@@ -14,6 +24,15 @@ const app = require('./app');
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+// it allow use to catch unhandled errors of async code
+process.on('unhandledRejection', (err: any) => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  //we give the server a chance to finish the request adn after the server killed
+  server.close(() => {
+    process.exit(1);
+  });
 });
